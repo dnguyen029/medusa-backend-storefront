2dd0b69724884b6e5e93908817b74e6acffd5ea1
dnguyen029 <dnguyen029@gmail.com>
2026-02-17 13:44:22 -0800

    chore: Add cdn.sanity.io to next.config.js for image optimization

--- next.config.js
+++ next.config.js
@@ -42,14 +42,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
       ...(S3_HOSTNAME && S3_PATHNAME
         ? [
-            {
-              protocol: "https",
-              hostname: S3_HOSTNAME,
-              pathname: S3_PATHNAME,
-            },
-          ]
+          {
+            protocol: "https",
+            hostname: S3_HOSTNAME,
+            pathname: S3_PATHNAME,
+          },
+        ]
         : []),
     ],
   },
